44d754e03cb0bcadd96dee3c68ca18112f772ccd
Finch-H <merchantdaniel8@gmail.com>
2022-09-09 19:47:01 +0000

    new files

---
+++ lib/homepage.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'dart:html';
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+
+class homepage extends StatefulWidget {
+  const homepage({Key? key}) : super(key: key);
+
+  @override
+  State<homepage> createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
